18ac888288ff08cb7f4ea0ecfbdd65fecaab12d0
xiaocode <337317439@qq.com>
2020-06-23 14:48:34 +0800

    xc

---
+++ src/views/Test/TestVue1.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <span>关于</span>
+    <RedPointComponent :redval="redCount">
+      <template scope='redCount'>
+        <i v-if="redCount.text > 0 && !showRedPoint" class="bg-danger redtip">
+          {{redCount.text >= 99 ? '99+' : redCount.text}}
+        </i>
+        <i v-else class="red-point"></i>
+      </template>
+    </RedPointComponent>
+  </div>
+</template>
+<script>
+  import RedPointComponent from './RedPointComponent'
+
+  export default {
+    data() {
+      return {
+        redCount: "10",
+        //是否只显示红点
+        showRedPoint: false
+      }
+    },
+    components: {
+      RedPointComponent
+    },
+  }
+</script>
+<style lang="scss" rel="stylesheet/scss">
+  .red-point {
+    position: relative;
+  }
+
+  .red-point::before {
+    content: " ";
+    border: 3px solid red; /*设置红色*/
+    border-radius: 3px; /*设置圆角*/
+    position: absolute;
+    z-index: 1000;
+    right: 0;
+    margin-right: -8px;
+  }
+
+  .redtip {
+    display: inline-block;
+    text-align: center;
+    background: #f74c31;
+    color: #fff;
+    font-size: 12px;
+    height: 16px;
+    line-height: 16px;
+    border-radius: 8px;
+    padding: 0 6px;
+    background-clip: padding-box;
+    vertical-align: middle;
+  }
+</style>
