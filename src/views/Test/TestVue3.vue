6498c6a330ab3a8a9b42baecb411cbe94b936e36
xiaocode <337317439@qq.com>
2020-08-06 13:44:20 +0800

    xc

---
+++ src/views/Test/TestVue3.vue
@@ -0,0 +1,46 @@
+<template>
+  <el-cascader
+    :options="options"
+    :props="props"
+    v-model="selectValue"
+    collapse-tags
+    clearable></el-cascader>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        props: { multiple: true },
+        selectValue: [],
+        options: [{
+          value: 1,
+          label: '东南',
+          children: [{
+            value: 2,
+            label: '上海',
+          }, {
+            value: 7,
+            label: '江苏',
+          }, {
+            value: 12,
+            label: '浙江'
+          }]
+        }, {
+          value: 17,
+          label: '西北',
+          children: [{
+            value: 18,
+            label: '陕西',
+          }, {
+            value: 21,
+            label: '新疆维吾尔族自治区',
+          }]
+        }]
+      };
+    },
+    mounted() {
+
+    }
+  };
+</script>
