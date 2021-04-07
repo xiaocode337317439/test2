db7f476a41fe123700d74f646709bafccaf866f6
xiaocode <337317439@qq.com>
2021-04-07 16:20:11 +0800

    xc

--- src/views/Test/TestVue.vue
+++ src/views/Test/TestVue.vue
@@ -7,7 +7,6 @@
     node-key="id"
     highlight-current
     :props="defaultProps"
-    @check-change="handleCheckChange"
   >
   </el-tree>
 </template>
@@ -58,29 +57,29 @@ export default {
       }
     }
   },
+  mounted() {
+    // 自动设置
+    this.$refs.tree.setCheckedKeys([3])
+
+    // 序列化this.data防止，改变原始数据， 因为this.data 传递到js方法是引用传递，会修改内存中的值
+    let copy = JSON.parse(JSON.stringify(this.data))
+    // 过滤所有选中没有孩子节点 即为最终勾选节点
+    const checkedNodes = this.$refs.tree.getCheckedNodes().filter(value => !value.children)
+    // 过滤数据
+    copy = this.filterData(copy, checkedNodes)
+    console.log(copy)
+  },
   methods: {
-    filter(data, checkedNodes) {
+    filterData(data, checkedNodes) {
       return data.filter(value => {
         if (value.children) {
           // 递归
-          value.children = this.filter(value.children, checkedNodes)
+          value.children = this.filterData(value.children, checkedNodes)
           return (value.children && value.children.length > 0)
         } else {
           return checkedNodes.find(findData => value.id === findData.id)
         }
       })
-    },
-    handleCheckChange(data, checked, indeterminate) {
-      if (!indeterminate && !data.children) {
-        // 被选中，子节点没有和节点数据没有children则判定为最终勾选的节点
-        // 序列化this.data防止，改变原始数据， 因为this.data 传递到js方法是引用传递，会修改内存中的值
-        let copy = JSON.parse(JSON.stringify(this.data))
-        // 过滤所有选中没有孩子节点 即为最终勾选节点
-        const checkedNodes = this.$refs.tree.getCheckedNodes().filter(value => !value.children)
-        // 过滤数据
-        copy = this.filter(copy, checkedNodes)
-        console.log(copy)
-      }
     }
   }
 }
